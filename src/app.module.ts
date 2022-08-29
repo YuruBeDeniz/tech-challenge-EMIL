df9a8190c38012d3d05f2c789505acb10c10e646
Deniz Saganak <saganak.deniz@gmail.com>
2022-08-29 22:03:17 +0200

    format setup, create post req and REST API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,17 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GpsdevicesModule } from './gpsdevices/gpsdevices.module';
-import { LoggerMiddleware } from './gpsdevices/middleware/logger.middleware';
-import { MongooseModule } from '@nestjs/mongoose';
+/* import { LoggerMiddleware } from './gpsdevices/middleware/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose'; */
 
 @Module({
   imports: [
     GpsdevicesModule,
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    // MongooseModule.forRoot('mongodb://localhost/nest'),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule implements NestModule {
+export class AppModule {} /* implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('gpsdevices');
     //we have set up the LoggerMiddleware for the /gpsdevices route handlers that were previously defined inside the gpsdevices.controller
   }
-}
+} */
--- src/gpsdevices/dto/create-gpsdevice.dto.ts
+++ src/gpsdevices/dto/create-gpsdevice.dto.ts
@@ -1,1 +1,18 @@
-export class CreateGpsdeviceDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateGpsdeviceDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  brand?: string;
+
+  @ApiProperty()
+  latitude: number;
+
+  @ApiProperty()
+  longitude: number;
+
+  @ApiProperty()
+  createdAt?: Date;
+}
