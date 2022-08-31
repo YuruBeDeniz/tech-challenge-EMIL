6b647e4c4c4372f00efbf908834f00558303b308
Deniz Saganak <saganak.deniz@gmail.com>
2022-08-31 12:26:54 +0200

    add README.md

--- src/gpsdevices/gpsdevices.module.ts
+++ src/gpsdevices/gpsdevices.module.ts
@@ -5,7 +5,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Gpsdevice, GpsdeviceSchema } from './schemas/gpddevice.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Gpsdevice.name, schema: GpsdeviceSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Gpsdevice.name, schema: GpsdeviceSchema },
+    ]),
+  ],
   controllers: [GpsdevicesController],
   providers: [GpsdevicesService],
 })
