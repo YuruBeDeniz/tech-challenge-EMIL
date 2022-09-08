73be8244ce1130e4232fce9eeb4c48584c3ed0e0
Deniz Saganak <saganak.deniz@gmail.com>
2022-09-08 12:55:47 +0200

    reformat structure

--- src/gpsdevices/gpsdevices.controller.ts
+++ src/gpsdevices/gpsdevices.controller.ts
@@ -1,13 +1,10 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { GpsdevicesService } from './gpsdevices.service';
 import { CreateGpsdeviceDto } from './dto/create-gpsdevice.dto';
 import { Request } from 'express';
 
 @Controller('/')
 export class GpsdevicesController {
-  static getHello(): any {
-    throw new Error('Method not implemented.');
-  }
   constructor(private readonly gpsdevicesService: GpsdevicesService) {}
 
   @Post('gpsdevices')
